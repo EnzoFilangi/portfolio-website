3ef91b1f4af035797574dfb340c76d66c21a422e
EnzoFilangi <enzo.filangi@efrei.net>
2022-08-08 23:18:53 +0200

    Configured Tailwind to respect the styles of the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['"Fira Code"', 'monospace'],
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#FAFAFA',
+        'black': '#2B2D2C',
+        'orange': '#D8814B',
+        'red': '#FF5858',
+        'blue': '#99C6FF',
+        'cyan-green': '#29D6B9',
+        'cyan-blue': '#28B8D5'
+      },
+    },
   },
   plugins: [],
 }
