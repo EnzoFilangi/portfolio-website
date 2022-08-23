5e8229525f0f426a12ff3c89be3af01cecf319bf
EnzoFilangi <enzo.filangi@efrei.net>
2022-08-23 13:03:26 +0200

    Fixed the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-exec >> /home/enzo/logs/webhooks/portfolio/output.log 2>>&1
+set -e
+exec >> /home/enzo/logs/webhooks/portfolio/output.log 2>>/home/enzo/logs/webhooks/portfolio/output.log
 
+echo -e "\n\n"
 echo "###### Deploying changes ######"
 date +'%Y-%m-%d %H:%M:%S'
+
+cd /home/enzo/portfolio-website
 
 echo "------ Git pull ------"
 git fetch --all
 git pull
 
 echo "------ Rebuilding and restarting the container ------"
-docker-compose up --force-recreate --build -d
+docker compose up --force-recreate --build -d
 
 echo "------ Deleting unused images ------"
 docker image prune -f
