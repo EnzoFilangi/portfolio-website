7e54e7a3f1d85d4eb1fab48f7846ade5fd466396
EnzoFilangi <enzo.filangi@efrei.net>
2022-08-26 15:51:43 +0200

    Updated tests so that 'ng test' would run without issue

--- src/app/components/general/navbar/navbar.component.spec.ts
+++ src/app/components/general/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {LandingPageComponent} from "../../pages/landing-page/landing-page.component";
+import {NotFoundComponent} from "../not-found/not-found.component";
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,7 +11,13 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([
+          {path: '', component: LandingPageComponent},
+          {path: '**', component: NotFoundComponent}
+        ])
+      ]
     })
     .compileComponents();
 
