3cbe0a5e9000c51c514fcd19ef693ee0c357d53a
EnzoFilangi <enzo.filangi@efrei.net>
2022-08-29 14:07:03 +0200

    Images grow when clicking on them

--- src/app/components/common/carousel/carousel.component.ts
+++ src/app/components/common/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
 import {CarouselItem} from "../../../interfaces/carousel-item";
 import {LanguageSwitcherService} from "../../../services/languageSwitcherService/language-switcher.service";
 import {Locales} from "../../../interfaces/enums/locales";
@@ -41,5 +41,35 @@ export class CarouselComponent {
 
   decrementSelected(): void {
     this.selected = Math.max(this.selected - 1, 0);
+  }
+
+  currentFullsizeImage: HTMLImageElement | null = null;
+  /**
+   * This method is used to make an image grow to its full size when clicking on it
+   * @param carouselImage
+   */
+  makeFullSize(carouselImage: HTMLImageElement) {
+    // We checke that the clicked image is different from the currently displayed image
+    // to prevent it from being dismissed and then grown again
+    if (this.currentFullsizeImage != carouselImage){
+      // Wait a few ms to avoid any possible race condition with the previous image
+      setTimeout(() => {
+        carouselImage.classList.add('full-size');
+        this.currentFullsizeImage = carouselImage;
+      }, 10)
+    }
+  }
+
+  /**
+   * Makes the current full size image go back to its normal size when clicking anywhere or scrolling
+   */
+  @HostListener('document:click')
+  @HostListener('document:scroll')
+  dismissFullSizeImage() {
+    // Check that the target of the click isn't contained in the button
+    if (this.currentFullsizeImage) {
+      this.currentFullsizeImage.classList.remove('full-size');
+      this.currentFullsizeImage = null;
+    }
   }
 }
