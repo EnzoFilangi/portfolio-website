d81c9654d2c46e1a831fb471616d8c32e1c7ba03
EnzoFilangi <enzo.filangi@efrei.net>
2022-09-08 10:12:42 +0200

    Gallery component + usage by dnd-gallery

--- src/app/components/common/gallery/gallery.component.ts
+++ src/app/components/common/gallery/gallery.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {LanguageSwitcherService} from "../../../services/languageSwitcherService/language-switcher.service";
+import {Locales} from "../../../interfaces/enums/locales";
+import {GalleryItem} from "../../../interfaces/gallery-item";
+import {GalleryContent} from "../../../interfaces/gallery-content";
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
 
-  constructor() { }
+  @Input() content : GalleryContent = {title_fr: "", title_en: "", gallery: []};
 
-  ngOnInit(): void {
+  constructor(
+    private languageSwitcherService: LanguageSwitcherService
+  ) { }
+
+  getTitle(content : GalleryContent): string {
+    switch (this.languageSwitcherService.getCurrentLocale()){
+      case Locales.fr_FR: return content.title_fr;
+      case Locales.en_US: return content.title_en;
+      default: return content.title_fr;
+    }
   }
 
+  /**
+   * Returns the entry's description in the right language
+   * @param entry
+   */
+  getDescription(entry: GalleryItem): string {
+    switch (this.languageSwitcherService.getCurrentLocale()){
+      case Locales.fr_FR: return entry.description_fr;
+      case Locales.en_US: return entry.description_en;
+      default: return entry.description_fr;
+    }
+  }
 }
---
+++ src/app/interfaces/gallery-content.ts
@@ -0,0 +1,7 @@
+import {GalleryItem} from "./gallery-item";
+
+export interface GalleryContent {
+  title_fr: string,
+  title_en: string,
+  gallery: GalleryItem[]
+}
