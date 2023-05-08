a2f7e445bcd027d620a5c9a6a7773009015ceaa7
EnzoFilangi <enzo.filangi@efrei.net>
2023-05-08 22:45:32 +0200

    Moved to using card stack in associative-experience-list.component instead of the old method

--- src/app/components/pages/landing-page/associative-experience-list/associative-experience-list.component.ts
+++ src/app/components/pages/landing-page/associative-experience-list/associative-experience-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-associative-experience-list',
@@ -8,57 +8,4 @@ import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 export class AssociativeExperienceListComponent {
 
   constructor() {}
-
-  @ViewChild('fapCard') fapCard!: ElementRef;
-
-  openedCard: HTMLDivElement | null = null;
-  /**
-   * Adds a class that makes the given element translate right, and removes the one that nudges it on hover.
-   *
-   * This method also stores the reference to close that card later, and closes any previously opened card.
-   * @param eventManagerCard
-   */
-  openCard(eventManagerCard: HTMLDivElement) {
-    if (this.openedCard){
-      this.closeCard(this.openedCard);
-    }
-
-    this.openedCard = eventManagerCard;
-    eventManagerCard.classList.add("translated-right");
-    eventManagerCard.classList.remove("open-slightly");
-  }
-
-  /**
-   * Removes the class that translates a card, and adds back the class that nudges it on hover.
-   * @param openedCard
-   * @private
-   */
-  private closeCard(openedCard: HTMLDivElement) {
-    openedCard.classList.remove("translated-right")
-    openedCard.classList.add("open-slightly")
-  }
-
-  /**
-   * If there is a card open, hide it when clicking anywhere else
-   * @param target
-   */
-  @HostListener('document:click', ['$event.target'])
-  hideNavigationMenu(target: Element) {
-    // Check that the target of the click isn't contained in the button
-    if (!this.fapCard.nativeElement.contains(target) && this.openedCard) {
-      this.closeCard(this.openedCard)
-      this.openedCard = null;
-    }
-  }
-
-  /**
-   * If there is a card open, hide it when scrolling
-   */
-  @HostListener('window:scroll', ['$event'])
-  applyShadowOnScroll() {
-    if (this.openedCard) {
-      this.closeCard(this.openedCard)
-      this.openedCard = null;
-    }
-  }
 }
